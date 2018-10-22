2b36646f3e98ddfa01723363bf58c1185cb97c2a
davidliu <david-khala@hotmail.com>
2018-10-22 13:06:25 +0800

    get/put private Obj

--- sideDB.go
+++ sideDB.go
@@ -1,14 +1,27 @@
 package golang
 
 import (
-	"github.com/hyperledger/fabric/core/chaincode/shim"
 	. "github.com/davidkhala/goutils"
+	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
 func (cc CommonChaincode) GetPrivateData(collection, key string) ([]byte) {
 	var r, err = cc.CCAPI.GetPrivateData(collection, key)
 	PanicError(err)
 	return r
+}
+func (cc CommonChaincode) GetPrivateObj(collection, key string, v interface{}) bool {
+	var r, err = cc.CCAPI.GetPrivateData(collection, key)
+	PanicError(err)
+	if r == nil {
+		return false
+	}
+	FromJson(r, v)
+	return true
+}
+func (cc CommonChaincode) PutPrivateObj(collection, key string, v interface{}) {
+	var err = cc.CCAPI.PutPrivateData(collection, key, ToJson(v))
+	PanicError(err)
 }
 func (cc CommonChaincode) PutPrivateData(collection, key string, value []byte) {
 	var err = cc.CCAPI.PutPrivateData(collection, key, value)
