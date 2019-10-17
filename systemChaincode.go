7753fd1fffc033d2447773b84d10d39ca41688d3
davidLiu <david-khala@hotmail.com>
2019-10-17 18:06:16 +0800

    not to panic before checking

--- systemChaincode.go
+++ systemChaincode.go
@@ -4,7 +4,7 @@ import "strings"
 
 func (cc CommonChaincode) ChaincodeExist(operationChannel, channel, checkedChaincode string) bool {
 	var args = [][]byte{[]byte("ChaincodeExists"), []byte(channel), []byte(checkedChaincode)}
-	var resp = cc.InvokeChaincode("lscc", args, operationChannel) // TODO could we specify another channel?
+	var resp = cc.CCAPI.InvokeChaincode("lscc", args, operationChannel) // TODO could we specify another channel?
 	//  {"status":500,"message":"could not find chaincode with name 'any'"
 	if resp.Status == 500 && strings.Contains(resp.Message, "could not find chaincode with name") {
 		return false
