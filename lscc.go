8b78178148b855841590050b0bc451df6bc3ec38
david <david-khala@hotmail.com>
2019-10-18 15:53:01 +0800

    [TODO] operational channel not used

---
+++ lscc.go
@@ -0,0 +1,34 @@
+package golang
+
+import (
+	. "github.com/davidkhala/goutils"
+	"github.com/golang/protobuf/proto"
+	"github.com/hyperledger/fabric/core/common/ccprovider"
+	"strings"
+)
+
+const operationChannel = "" // It does not matter what channel we use, so use current channel
+
+// TODO the meaning of ChaincodeExist
+// query function, a short representation of GetChaincodeData
+func (cc CommonChaincode) ChaincodeExist(channel, checkedChaincode string) bool {
+	var args = [][]byte{[]byte("ChaincodeExists"), []byte(channel), []byte(checkedChaincode)}
+	var resp = cc.CCAPI.InvokeChaincode("lscc", args, operationChannel)
+	//  {"status":500,"message":"could not find chaincode with name 'any'"
+	if resp.Status == 500 && strings.Contains(resp.Message, "could not find chaincode with name") {
+		return false
+	} else {
+		PanicPeerResponse(resp)
+	}
+	return true
+
+}
+func (cc CommonChaincode) GetChaincodeData(channel, checkedChaincode string) []byte {
+	var args = [][]byte{[]byte("GetChaincodeData"), []byte(channel), []byte(checkedChaincode)}
+	var resp = cc.InvokeChaincode("lscc", args, operationChannel)
+
+	var chaincodeData = &ccprovider.ChaincodeData{}
+	var err = proto.Unmarshal(resp.Payload, chaincodeData)
+	PanicError(err)
+	return resp.Payload
+}
