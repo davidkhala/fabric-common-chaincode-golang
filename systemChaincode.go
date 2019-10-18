d1167dce1dd2ff206234974f03d97d97575b72f2
david <david-khala@hotmail.com>
2019-10-18 15:01:55 +0800

    confusing ChaincodeExist

--- systemChaincode.go
+++ systemChaincode.go
@@ -2,9 +2,10 @@ package golang
 
 import "strings"
 
+// TODO the meaning of ChaincodeExist
 func (cc CommonChaincode) ChaincodeExist(operationChannel, channel, checkedChaincode string) bool {
 	var args = [][]byte{[]byte("ChaincodeExists"), []byte(channel), []byte(checkedChaincode)}
-	var resp = cc.CCAPI.InvokeChaincode("lscc", args, operationChannel) // TODO could we specify another channel?
+	var resp = cc.CCAPI.InvokeChaincode("lscc", args, operationChannel)
 	//  {"status":500,"message":"could not find chaincode with name 'any'"
 	if resp.Status == 500 && strings.Contains(resp.Message, "could not find chaincode with name") {
 		return false
