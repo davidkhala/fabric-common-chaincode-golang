103ff2b41bb988df9ad0df128921b01fb340f31d
davidLiu <david-khala@hotmail.com>
2019-10-21 17:43:57 +0800

    add structure ChaincodeData

--- lscc.go
+++ lscc.go
@@ -4,6 +4,7 @@ import (
 	. "github.com/davidkhala/goutils"
 	"github.com/golang/protobuf/proto"
 	"github.com/hyperledger/fabric/core/common/ccprovider"
+	"github.com/hyperledger/fabric/protos/common"
 	"strings"
 )
 
@@ -22,12 +23,44 @@ func (cc CommonChaincode) ChaincodeExist(channel, checkedChaincode string) bool 
 	return true
 
 }
-func (cc CommonChaincode) GetChaincodeData(channel, checkedChaincode string) ccprovider.ChaincodeData {
+
+type ChaincodeData struct {
+	Name                string                         // Name of the chaincode
+	Version             string                         // Version of the chaincode
+	Escc                string                         // Escc for the chaincode instance
+	Vscc                string                         // Vscc for the chaincode instance
+	Data                ccprovider.CDSData             // Data data specific to the package
+	Policy              common.SignaturePolicyEnvelope // Policy endorsement policy for the chaincode instance
+	InstantiationPolicy common.SignaturePolicyEnvelope // InstantiationPolicy for the chaincode
+}
+
+//type SignaturePolicyEnvelope struct {
+//	Version    int32
+//	Rule       *common.SignaturePolicy
+//	Identities []*msp.MSPPrincipal
+//}
+
+func (cc CommonChaincode) GetChaincodeData(channel, checkedChaincode string) ChaincodeData {
 	var args = [][]byte{[]byte("GetChaincodeData"), []byte(channel), []byte(checkedChaincode)}
 	var resp = cc.InvokeChaincode("lscc", args, operationChannel)
 
 	var chaincodeData = ccprovider.ChaincodeData{}
 	var err = proto.Unmarshal(resp.Payload, &chaincodeData)
 	PanicError(err)
-	return chaincodeData
+
+	var policyProto = common.SignaturePolicyEnvelope{}
+	PanicError(proto.Unmarshal(chaincodeData.Policy, &policyProto)) //TODO TBC  [principal:"\n\010ASTRIMSP"  principal:"\n\007icddMSP" ]
+
+	var instantiatePolicyProto = common.SignaturePolicyEnvelope{}
+	PanicError(proto.Unmarshal(chaincodeData.InstantiationPolicy, &instantiatePolicyProto))
+
+	var convertedChaincodeData = ChaincodeData{
+		Name:                chaincodeData.Name,
+		Version:             chaincodeData.Version,
+		Escc:                chaincodeData.Escc,
+		Vscc:                chaincodeData.Vscc,
+		Policy:              policyProto,
+		InstantiationPolicy: instantiatePolicyProto,
+	}
+	return convertedChaincodeData
 }
